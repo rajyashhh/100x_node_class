d3d9d131aeba734346d952c74789ca1404824614
Yash Raj Singh <workmail.yrs@gmail.com>
2025-02-16 20:06:54 +0530

    Added Commander CLI function

--- cli.js
+++ cli.js
@@ -5,6 +5,48 @@
 //Input - node index.js /Users/yashajsingh/file.txt
 //Output - You have 10 words in this file.
 
-const { program } = require('commander');
+const { Command } = require('commander');
+const program = new Command();
 const fs= require('fs');
 
+
+
+program
+  .name('counter')
+  .description('CLI to count words in a file')
+  .version('0.1.1');
+
+program.command('count_words')
+  .description('Counts the number of words in a file')
+  .argument('<file>', 'file in whoch word has to be counted')
+  
+  .action(((fileName)=>{
+    fs.readFile(fileName, "utf-8", function(err,data){
+        let total = 0
+        for(let i=0; i<data.length; i++){
+            if(data[i]===" "){
+                total++;
+            }
+        }
+        console.log(`There are total ${total} number of words in ${fileName}.`)
+    })
+
+  }));
+  program.command('count_lines')
+  .description('Counts the number of lines in a file')
+  .argument('<file>', 'file in whoch word has to be counted')
+  
+  .action(((fileName)=>{
+    fs.readFile(fileName, "utf-8", function(err,data){
+        let total = 0
+        for(let i=0; i<data.length; i++){
+            if(data[i]==="\n"){
+                total++;
+            }
+        }
+        console.log(`There are total ${total+1} number of lines in ${fileName}.`)
+    })
+
+  }));
+
+program.parse();
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-console.log("Hey Baby!");
-console.log(__dirname);
-console.log(path.join(__dirname,"../..index.js", "node"))
+// console.log("Hey Baby!");
+// console.log(__dirname);
+// console.log(path.join(__dirname,"../..index.js", "node"))
+
+const fs = require("fs");
+
+function main(fileName){
+    fs.readFile(fileName, "utf-8", function(err,data){
+        let total = 0
+        for(let i=0; i<data.length; i++){
+            if(data[i]===" "){
+                total++;
+            }
+        }
+        console.log(total+1)
+    })
+}
+main(process.argv[2]);
